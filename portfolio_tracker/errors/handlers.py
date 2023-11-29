25da74e665c97933dd523a9fd273e6b8bb91aba5
Nikita <mr.nik2014@gmail.com>
2023-11-29 08:43:47 +0300

    refactoring Errors

--- portfolio_tracker/errors/handlers.py
+++ portfolio_tracker/errors/handlers.py
@@ -4,32 +4,32 @@ from portfolio_tracker.app import db
 from portfolio_tracker.errors import bp
 
 
-# 404 - Page Not Found
 @bp.app_errorhandler(404)
-def page_not_found(error):
+def page_not_found(_):
+    """404 - Page Not Found."""
     return render_template('errors/404.html'), 404
 
 
-# 500 - Internal Server Error
 @bp.app_errorhandler(500)
-def internal_error(error):
+def internal_error(_):
+    """500 - Internal Server Error."""
     db.session.rollback()
     return render_template('errors/500.html'), 500
 
 
-# 400 - Bad Request
 @bp.errorhandler(400)
-def bad_request(e):
+def bad_request(_):
+    """400 - Bad Request."""
     return render_template('errors/400.html'), 400
 
 
-# 403 - Forbidden
 @bp.errorhandler(403)
-def forbidden(e):
+def forbidden(_):
+    """403 - Forbidden."""
     return render_template('errors/403.html'), 403
 
 
-# 405 - Method Not Allowed
 @bp.errorhandler(405)
-def method_not_allowed(e):
+def method_not_allowed(_):
+    """405 - Method Not Allowed."""
     return render_template('errors/405.html'), 405
