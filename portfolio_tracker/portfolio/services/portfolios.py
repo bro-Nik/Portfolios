c3e97e03013eff9faad1ab618c088e2c1731dcac
Nikita Markin <mr.nik2014@gmail.com>
2025-02-06 14:04:53 +0300

    FIX: portfolio.services.portfolios fixed import

--- portfolio_tracker/portfolio/services/portfolios.py
+++ portfolio_tracker/portfolio/services/portfolios.py
@@ -1,7 +1,7 @@
 from __future__ import annotations
 from typing import TYPE_CHECKING
 
-from ..mixins import DetailsMixin
+from ..models import DetailsMixin
 
 if TYPE_CHECKING:
     from portfolio_tracker.user.models import User
