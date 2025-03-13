5f48f7a501b7786799ec5f5b152f88164012ca5d
Nikita Markin <mr.nik2014@gmail.com>
2025-03-13 17:55:32 +0300

    ADD: CI/CD settings

---
+++ deploy.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+echo "Knocking on ports..."
+for x in $SSH_PORTS; do nmap -Pn --max-retries 0 -p $x $SSH_HOST; done
+
+echo "Connecting to SSH..."
+ssh -o StrictHostKeyChecking=no -i $SSH_PRIVATE_KEY $SSH_USERNAME@$SSH_HOST << EOF
+  cd /home/nik/portfolios
+  git pull
+  python -m pip install --upgrade pip
+  pip install -r requirements.txt
+  sudo systemctl restart portfolios
+  sudo systemctl restart celery
+  sudo systemctl restart nginx
+EOF
+
+echo "Deployment completed!"
