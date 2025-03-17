6d637f81dff4bc6457f1bdab4ee286d85e6029cc
Nikita Markin <mr.nik2014@gmail.com>
2025-03-17 13:41:14 +0300

    FIX: CI/CD test setup

--- deploy.sh
+++ deploy.sh
@@ -4,7 +4,7 @@ echo "Knocking on ports..."
 for x in $SSH_PORTS; do nmap -Pn --max-retries 0 -p $x $SSH_HOST; done
 
 echo "Connecting to SSH..."
-ssh -o StrictHostKeyChecking=no -i $SSH_PRIVATE_KEY $SSH_USERNAME@$SSH_HOST -p $SSH_PORT << EOF
+ssh -tt -o StrictHostKeyChecking=no -i $SSH_PRIVATE_KEY $SSH_USERNAME@$SSH_HOST -p $SSH_PORT << EOF
   cd /home/nik/portfolios
   git pull
   python -m pip install --upgrade pip
