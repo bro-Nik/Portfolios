e5c65dcb0fb45e3ef737d1ceb1ebe6529240c034
Nikita Markin <mr.nik2014@gmail.com>
2025-03-18 15:15:48 +0300

    FIX: CI/CD final configuration

--- deploy.sh
+++ deploy.sh
@@ -15,26 +15,23 @@ echo "Connecting to SSH..."
 eval "$(ssh-agent -s)"
 echo "$SSH_KEY_PASSPHRASE" | ssh-add ~/.ssh/id_ed25519
 
-if ! ssh -v -o StrictHostKeyChecking=no $SSH_USERNAME@$SSH_HOST -p $SSH_PORT << EOF
+if ! ssh -o StrictHostKeyChecking=no $SSH_USERNAME@$SSH_HOST -p $SSH_PORT << EOF
   cd /home/nik/portfolios
   git pull
   python3 -m pip install --upgrade pip
   pip install -r requirements.txt
-  echo "$SUDO_PASSWORD" | sudo -S systemctl restart portfolios
-  echo "$SUDO_PASSWORD" | sudo -S systemctl restart celery
-  echo "$SUDO_PASSWORD" | sudo -S systemctl restart nginx
 
-  if ! echo "***" | sudo -S systemctl restart portfolios; then
+  if ! echo "$SUDO_PASSWORD" | sudo -S systemctl restart portfolios; then
     echo "Failed to restart portfolios!"
     exit 1
   fi
 
-  if ! echo "***" | sudo -S systemctl restart celery; then
+  if ! echo "$SUDO_PASSWORD" | sudo -S systemctl restart celery; then
     echo "Failed to restart celery!"
     exit 1
   fi
 
-  if ! echo "***" | sudo -S systemctl restart nginx; then
+  if ! echo "$SUDO_PASSWORD" | sudo -S systemctl restart nginx; then
     echo "Failed to restart nginx!"
     exit 1
   fi
